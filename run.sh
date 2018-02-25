30cef82ac337d63a22bd1c7a894292b952d6c832
clayriley <riley@brandeis.edu>
2018-02-25 00:08:41 -0500

    Print model prediction output path

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 now=$(date +%Y%m%d%H%M%S)
-model="model$now"".pred"
-python model.py --train data/en_es/en_es.slam.20171218.train --test data/en_es/en_es.slam.20171218.dev --pred out/en_es/$model
+model="out/en_es/model$now"".pred"
+echo "training and writing test predictions to $model""..."
+python model.py --train data/en_es/en_es.slam.20171218.train --test data/en_es/en_es.slam.20171218.dev --pred $model
 wait;
-python eval.py --pred out/en_es/$model --key data/en_es/en_es.slam.20171218.dev.key 
+python eval.py --pred $model --key data/en_es/en_es.slam.20171218.dev.key 
